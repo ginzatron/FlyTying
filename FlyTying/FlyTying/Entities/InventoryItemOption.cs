47668c3dbb7a00fd415f3ec59657668278d2008f
ginzatron <43377218+ginzatron@users.noreply.github.com>
2020-08-19 11:40:50 -0400

    Navigational property on Inventory Category to point to all InventoryItems in the category

--- FlyTying/FlyTying/Entities/InventoryItemOption.cs
+++ FlyTying/FlyTying/Entities/InventoryItemOption.cs
@@ -9,7 +9,5 @@ namespace FlyTying.Entities
     {
         public string Discriminator { get; set; } // size, color, weight etc. Should this be it's own class ItemOptionCategory?
         public string Value { get; set; }
-
-        // not sure if this needs the InventoryItem InventoryItem {get; set;} navigational property
     }
 }
