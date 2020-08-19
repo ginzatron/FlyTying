caab0154ab2ab79257070f5ab54af1fb460a62c0
ginzatron <43377218+ginzatron@users.noreply.github.com>
2020-08-19 14:22:13 -0400

    Merge pull request #7
    * Created AppUser class

---
+++ FlyTying/FlyTying/Entities/AppUser.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FlyTying.Entities
+{
+    public class AppUser : BaseEntity
+    {
+        public string Email { get; set; }
+        public string SubjectId { get; set; }
+
+        public IEnumerable<Fly> UserFlys { get; set; }
+    }
+}
