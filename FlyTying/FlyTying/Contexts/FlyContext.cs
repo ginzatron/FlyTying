0e158c056d120e37b2a07502c9da1bacd6d617b2
ginzatron <43377218+ginzatron@users.noreply.github.com>
2020-08-19 17:23:51 -0400

    Setting up fly context

---
+++ FlyTying/FlyTying/Contexts/FlyContext.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using FlyTying.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace FlyTying.Contexts
+{
+    public class FlyContext : DbContext
+    {
+        public FlyContext(DbContextOptions<FlyContext> options)
+            : base(options)
+        {
+        }
+        
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+        }
+
+        public DbSet<Fly> Flies { get; set; }
+        public DbSet<FlyCategory> FlyCategories { get; set; }
+        public DbSet<FlyMaterial> FlyMaterials { get; set; }
+        public DbSet<FlyMaterialOption> FlyMaterialOptions { get; set; }
+        public DbSet<FlyMaterialPosition> FlyMaterialPositions { get; set; }
+    }
+}
