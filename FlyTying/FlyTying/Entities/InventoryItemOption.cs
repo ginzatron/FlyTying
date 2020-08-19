3a93518ae708c15f5b71777f9e317c6d86c453b7
ginzatron <43377218+ginzatron@users.noreply.github.com>
2020-08-18 23:16:58 -0400

    added discriminatory to InventoryItemOption

--- FlyTying/FlyTying/Entities/InventoryItemOption.cs
+++ FlyTying/FlyTying/Entities/InventoryItemOption.cs
@@ -8,6 +8,7 @@ namespace FlyTying.Entities
     public class InventoryItemOption
     {
         public Guid ItemOptionId { get; set; }
+        public string Discriminator { get; set; } // size, color, weight etc. Should this be it's own class ItemOptionCategory?
         public string Value { get; set; }
 
         // not sure if this needs the InventoryItem InventoryItem {get; set;} navigational property
