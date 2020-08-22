fa239ca834365fb6759877525498489c53cab947
ginzatron <43377218+ginzatron@users.noreply.github.com>
2020-08-22 14:27:42 -0400

    Added new context, AppUser and Material and registered them in the startup class

---
+++ FlyTying/FlyTying/Contexts/MaterialContext.cs
@@ -0,0 +1,25 @@
+﻿using FlyTying.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FlyTying.Contexts
+{
+    public class MaterialContext : DbContext
+    {
+        public MaterialContext(DbContextOptions<MaterialContext> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+        }
+
+        public DbSet<Material> Materials { get; set; }
+        public DbSet<MaterialOption> MaterialOptions { get; set; }
+        public DbSet<MaterialCategory> MaterialCategories { get; set; }
+    }
+}
