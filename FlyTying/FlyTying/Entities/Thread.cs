86d81c3aab984f82592afeb7c36063c7b5ba9a55
ginzatron <43377218+ginzatron@users.noreply.github.com>
2020-08-30 15:19:15 -0400

    adding properties to domain models

--- FlyTying/FlyTying/Entities/Thread.cs
+++ FlyTying/FlyTying/Entities/Thread.cs
@@ -8,7 +8,11 @@ namespace FlyTying.Entities
     public class Thread
     {
         public string Weight { get; set; }
-        public string Denier { get; set; }
+        public string Denier { get; set; } // more accurate weight measure, weight in grams of 9k meters of thread
+        public string BreakingStrenth { get; set; } // in oz. don't see that much
+        public string Fiber { get; set; } // nylon, silk, polyester, kevlar, GSP? most people use nylon or heavier polyester
+        public bool Waxed { get; set; } // everything except, silk kevlar and GSP come waxed
+        public string Configuration { get; set; } // round or flat
         public IEnumerable<string> ColorOptions { get; set; }
     }
 }
