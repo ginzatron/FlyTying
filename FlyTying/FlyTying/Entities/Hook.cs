1cfec3a650101f8604d9dea837464394616f1474
ginzatron <43377218+ginzatron@users.noreply.github.com>
2020-08-31 17:21:23 -0400

    removed domain folder

--- FlyTying/FlyTying/Entities/Hook.cs
+++ FlyTying/FlyTying/Entities/Hook.cs
@@ -8,25 +8,29 @@ namespace FlyTying.Entities
     public class Hook : EntityBase
     {
         public string BrandName { get; set; }
-        public string SizeRange { get; set; }
-        public string Eye { get; set; }
+        public string Size { get; set; }
+        public string Eye { get; set; } // down, up, straight
+
+        // maybe structure an enum if 1x,2x,3x,4x then add a "modifier" so you know what it's for
+        // ie. long/short, heay/fine, strong/weak, wide
 
         // hook length will be it's own class where you denote length, standard, XLong or Xshort
         public string Length { get; set; } // #12 hook that is 2X Long it will have the wire strength of a #12 but the length of a #8(two sizes bigger)
+        // maybe an enum: 1x 2x 3x 4x
 
         // weight is its own class
         public string Weight { get; set; } // 1x Fine uses wire from one size smaller, 2x heavy, with wire two sizes bigger
         // denotes a wire strength of a larger hook but on the current hook, ex 1xStrong hook of size #6 will have the wire strength of a #4
         // sometimes referred to with XStrong or XStout
 
-        public string Wire { get; set; } 
+        public string Wire { get; set; } // not sure if this is the same as weight
         public string Gape { get; set; } // comes in standard.  1x wide would have the gape of a standard hook 1 size bigger
         public string Metal { get; set; } // black-nickel, bronze
-        public string Point { get; set; } // point, rounded
+        public string Point { get; set; } // point, rounded, semi-dropped, dropped
         public string Shank { get; set; } // long, short, curved, straight
         public bool Barb { get; set; }
         public string GapWidth { get; set; } 
-        public string Bend { get; set; } // perfect, reversed, Limerick
+        public string Bend { get; set; } // perfect, reversed, Limerick, sproat, klinkhamer
         
         // wire gauge table/object, AWG or SWG size then diamter measurements
         //https://en.wikipedia.org/wiki/American_wire_gauge
@@ -39,4 +43,6 @@ namespace FlyTying.Entities
     // streamers being sizes 4-12 and then 3x/4x long and 1x/2x heavy
 
     // nymph hooks as standard i assume will have a longer length to begin with?
+
+    // the larger hooks get, the sparser the details
 }
