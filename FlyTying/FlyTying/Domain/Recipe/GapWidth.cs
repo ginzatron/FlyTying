e1e1ec3b5654e1acbc4933557bdc42e303f77aea
ginzatron <43377218+ginzatron@users.noreply.github.com>
2020-09-16 17:19:48 -0400

    refactor properties

---
+++ FlyTying/FlyTying/Domain/Recipe/GapWidth.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FlyTying.Domain.Recipe
+{
+    public enum GapWidth
+    {
+        Standard,
+        Wide,
+        Narrow
+    }
+}
