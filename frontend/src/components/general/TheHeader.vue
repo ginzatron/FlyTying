5cecd97461f611506e8c8f61687d67accd151680
ginzatron <43377218+ginzatron@users.noreply.github.com>
2020-12-06 15:44:14 -0500

    added front end vue project

---
+++ frontend/src/components/general/TheHeader.vue
@@ -0,0 +1,74 @@
+<template>
+  <header>
+      <div class="logo">THE VISE LOGO</div>
+      <!-- <div id="g-signin2" data-onsuccess="onSignIn"></div>
+      <a href="#" onclick="signOut();">Sign out</a> -->
+      <nav>
+        <ul class="nav-links">
+          <li id="getFlys"><router-link to="/flys">Flys</router-link></li>
+          <li><a href="#">Create Flys</a></li>
+        </ul>
+      </nav>
+      <a class="button" href="#" type="button">Contact</a>
+    </header>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+header {
+  display: flex;
+  justify-content: flex-end;
+  align-items: center;
+  padding: 30px 10%;
+}
+
+.logo {
+  cursor: pointer;
+  margin-right: auto;
+}
+
+.nav-links {
+  list-style: none;
+}
+
+.nav-links li {
+  display: inline-block;
+  padding: 0px 20px;
+}
+
+.nav-links li a {
+  transition: all 0.3s ease 0s;
+}
+
+.nav-links li a:hover {
+  color: #0088a9;
+}
+
+.button {
+  margin-left: 20px;
+  padding: 9px 25px;
+  background-color: rgba(0, 136, 169, 1);
+  border: none;
+  border-radius: 50px;
+  cursor: pointer;
+  transition: all 0.3s ease 0s;
+}
+
+.button:hover {
+  background-color: rgba(0, 136, 169, 0.8);
+}
+
+.flyName .flyClass {
+  color: white;
+}
+
+.createFlySection {
+  display: flex;
+  justify-content: center;
+}
+</style>
