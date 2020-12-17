685887d7f8aac8b2c4fe9e7ce10b9da183ea2bee
ginzatron <43377218+ginzatron@users.noreply.github.com>
2020-12-17 10:43:28 -0500

    created routing from Flys to fly. added route building method

--- frontend/src/components/fly/FlyDetail.vue
+++ frontend/src/components/fly/FlyDetail.vue
@@ -1,47 +1,46 @@
 <template>
-  <h1>{{st}}</h1>
-  <h2>{{ payload.data.name}}</h2>
+  <h2>{{ payload.data.name }}</h2>
 </template>
 
 <script>
-import {ref, reactive, onMounted} from 'vue';
+import { reactive, onMounted } from "vue";
 
 export default {
-    props: ['id'],
-    setup(props){
-        const st = ref('test');
-        const payload = reactive({
-            loading: false,
-            error: '',
-            data: {}
-        });
-        
-        async function getFly() {
-            payload.loading = true;
-            payload.data = {};
-            const response = await fetch(`https://localhost:44352/api/recipes/${props.id}`,{
-                headers: {
-                    accept: 'application/json',
-                }
-            });
-            if(!response.ok){
-                payload.error = 'could not complete request';
-            }
-            const data = await response.json();
-            payload.loading = false;
-            payload.data = data;
-        }
+  props: ["id"],
+  setup(props) {
+    const payload = reactive({
+      loading: false,
+      error: "",
+      data: {},
+    });
 
-        onMounted(getFly);
-
-        return {
-            payload,
-            st,
+    async function getFly() {
+      payload.loading = true;
+      payload.data = {};
+      const response = await fetch(
+        `https://localhost:44352/api/recipes/${props.id}`,
+        {
+          headers: {
+            accept: "application/json",
+          },
         }
+      );
+      if (!response.ok) {
+        payload.error = "could not complete request";
+      }
+      const data = await response.json();
+      payload.loading = false;
+      payload.data = data;
     }
-}
+
+    onMounted(getFly);
+
+    return {
+      payload,
+    };
+  },
+};
 </script>
 
 <style>
-
 </style>
