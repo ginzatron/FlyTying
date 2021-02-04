7746441ec88cd4ef1861eac2e2e79e62b7f6f3f3
ginzatron <43377218+ginzatron@users.noreply.github.com>
2021-02-04 14:43:11 -0500

    refactoring facet creation to be more dynamic and refactoring looping code

--- FlyTying/FlyTying/Application/Repositories/RecipeRepository.cs
+++ FlyTying/FlyTying/Application/Repositories/RecipeRepository.cs
@@ -14,6 +14,7 @@ namespace FlyTying.Application.Repositories
     public class RecipeRepository : MongoAsyncRepository<Recipe>, IRecipeRepository
     {
         private readonly MongoRecipeDBContext _context;
+        private static readonly string[] _facets = {"Hook.Classifiacation", "Pattern.PatternType", "HookSizes"};
 
         public RecipeRepository(MongoRecipeDBContext context)
             : base(context)
@@ -33,119 +34,85 @@ namespace FlyTying.Application.Repositories
             var returnSet = new UpdatedFacetResults()
             {
                 Recipes = matchResult.ToList(),
-                Facets = searchFacets
+                Facets = searchFacets.ToList()
             };
 
             return returnSet;
         }
 
-        private async Task<List<SearchFacet>> GenerateSearchFacets(IAggregateFluent<Recipe> matchResult, IEnumerable<SearchFacet> selectedFacets)
+        private async Task<IEnumerable<SearchFacet>> GenerateSearchFacets(IAggregateFluent<Recipe> matchResult, IEnumerable<SearchFacet> selectedFacets)
         {
-            var hookFacet = CreateHookFacet();
-            var patternFacet = CreatePatternFacet();
-            var hookSizeFacet = CreateHookSizeFacet();
+            var searechFacets = await matchResult.Facet(CreateFacet().ToList()).ToListAsync();
+            var results =  searechFacets.First().Facets.Select(x => new { Group = x.Name, Facets = x.Output<AggregateSortByCountResult<string>>() });
 
-            var searechFacets = await matchResult.Facet(hookFacet, patternFacet, hookSizeFacet).ToListAsync();
-            var x =  searechFacets.First().Facets.Select(x => new { Group = x.Name, Facets = x.Output<AggregateSortByCountResult<string>>() });
+            return from result in results
+                    from facet in result.Facets
+                    select 
+                    (new SearchFacet { 
+                        Count = (Int32)facet.Count, 
+                        Title = facet.Id, 
+                        Group = result.Group, 
+                        Selected = (selectedFacets.Any(x => x.Group == result.Group && x.Title == facet.Id))
+                    });
 
-            var facetList = new List<SearchFacet>();
 
-            foreach(var record in x)
+    
+                    
+            //var facetList = new List<SearchFacet>();
+
+            //foreach (var record in results)
+            //{
+            //    var facet = record.Facets;
+            //    foreach (var f in facet)
+            //    {
+            //        var searchFacet = new SearchFacet()
+            //        {
+            //            Count = (Int32)f.Count,
+            //            Title = f.Id,
+            //            Group = record.Group,
+            //            Selected = false
+            //        };
+
+            //        if (selectedFacets.Any(x => x.Group == searchFacet.Group && x.Title == searchFacet.Title))
+            //            searchFacet.Selected = true;
+
+            //        facetList.Add(searchFacet);
+            //    }
+            //}
+
+            //return facetList;
+        }
+
+        private IEnumerable<AggregateFacet<Recipe, AggregateSortByCountResult<string>>> CreateFacet()
+        {
+            foreach(var facet in _facets)
             {
-                var facet = record.Facets;
-                foreach(var f in facet)
+                yield return AggregateFacet.Create(facet, PipelineDefinition<Recipe, AggregateSortByCountResult<string>>.Create(new[]
                 {
-                    var searchFacet = new SearchFacet()
-                    {
-                        Count = (Int32)f.Count,
-                        Title = f.Id,
-                        Group = record.Group,
-                        Selected = false
-                    };
-
-                    if (selectedFacets.Any(x => x.Group == searchFacet.Group && x.Title == searchFacet.Title))
-                        searchFacet.Selected = true;
-
-                    facetList.Add(searchFacet);
-                }
+                    PipelineStageDefinitionBuilder.SortByCount<Recipe, string>($"${facet}")
+                }));
             }
-
-            return facetList;
         }
-
-        private AggregateFacet<Recipe, AggregateSortByCountResult<string>> CreatePatternFacet()
-        {
-            var patternFacet = AggregateFacet.Create("PatternTypes",
-            PipelineDefinition<Recipe, AggregateSortByCountResult<string>>.Create(new[]
-            {
-                PipelineStageDefinitionBuilder.SortByCount<Recipe, string>("$Pattern.PatternType")
-            }));
-
-            return patternFacet;
-        }
-
-        private AggregateFacet<Recipe,AggregateSortByCountResult<string>> CreateHookFacet()
-        {
-            var hookFacet = AggregateFacet.Create("HookClassifications",
-            PipelineDefinition<Recipe, AggregateSortByCountResult<string>>.Create(new[]
-            {
-                PipelineStageDefinitionBuilder.SortByCount<Recipe, string>("$Hook.Classification")
-            }));
-
-            return hookFacet;
-        }
-
-        private AggregateFacet<Recipe, AggregateSortByCountResult<string>> CreateHookSizeFacet()
-        {
-            var hookFacet = AggregateFacet.Create("HookSizes",
-            PipelineDefinition<Recipe, AggregateSortByCountResult<string>>.Create(new[]
-            {
-                PipelineStageDefinitionBuilder.SortByCount<Recipe, string>("$Hook.Size")
-            }));
-
-            return hookFacet;
-        }
-
         private FilterDefinition<Recipe> BuildFilterFromFacets(IEnumerable<SearchFacet> facets)
         {
-            // this is not an elegant or efficient way to do this
             var filter = Builders<Recipe>.Filter.Empty;
-            var facetDictionary = new Dictionary<string, List<string>>();
 
-            foreach(var record in facets)
+
+            var facetDictionary = (from facet in facets
+                                   group facet.Title by facet.Group
+                                   into dict
+                                   select dict).ToDictionary(x => x.Key, x => x.ToArray());
+
+            foreach(var keyValuePair in facetDictionary)
             {
-                if (!facetDictionary.ContainsKey(record.Group))
-                {
-                    facetDictionary[record.Group] = new List<string>() { record.Title };
-                }
-                else
-                {
-                    facetDictionary[record.Group].Add(record.Title);
-                }
+                filter &= Builders<Recipe>.Filter.In(keyValuePair.Key, keyValuePair.Value);
             }
-
-            if (facetDictionary.ContainsKey("PatternTypes"))
-            {
-                var patternArray = facetDictionary["PatternTypes"].Select(x => (PatternType)Enum.Parse(typeof(PatternType), x));
-                var patternFilter = Builders<Recipe>.Filter.In(x => x.Pattern.PatternType, patternArray);
-                filter &= patternFilter;
-            }
-
-            if (facetDictionary.ContainsKey("HookClassifications"))
-            {
-                var hookArray = facetDictionary["HookClassifications"].Select(x => (HookClassification)Enum.Parse(typeof(HookClassification), x));
-                var hookClassFilter = Builders<Recipe>.Filter.In(x => x.Hook.Classification, hookArray);
-                filter &= hookClassFilter;
-            }
-
-            if (facetDictionary.ContainsKey("HookSizes"))
-            {
-                var hookSizeFilter = Builders<Recipe>.Filter.In(x => x.Hook.Size, facetDictionary["HookSizes"]);
-                filter &= hookSizeFilter;
-            }
-
+                               
             return filter;
         }
+
+        //mapping class => dictionary <facetGroup, enum type and property path>
+        // 
     }
 }
 
