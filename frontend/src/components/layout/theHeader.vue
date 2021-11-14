1ae85d70e38dbb9851e5a76f11f1604e068f5f9f
ginzatron <43377218+ginzatron@users.noreply.github.com>
2021-11-13 21:22:32 -0600

    added quasar and some test componetns

---
+++ frontend/src/components/layout/theHeader.vue
@@ -0,0 +1,9 @@
+<template>
+    <div>{{ msg }}</div>
+</template>
+
+<script setup lang="ts">
+const props = defineProps({
+    msg: { type: String, default: 'default message' },
+})
+</script>
--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,4 +1,16 @@
 import { createApp } from 'vue'
+import { Quasar } from 'quasar'
+
+import '@quasar/extras/material-icons/material-icons.css'
+import '@quasar/extras/fontawesome-v5/fontawesome-v5.css'
+
+import 'quasar/src/css/index.sass'
+
 import App from './App.vue'
 
-createApp(App).mount('#app')
+const myApp = createApp(App)
+myApp.use(Quasar, {
+    plugins: {}, // import Quasar plugins and add here
+})
+
+myApp.mount('#app')
