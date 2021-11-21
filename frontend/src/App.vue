767e5b065864bfd960a93484118d914059a12e8d
ginzatron <43377218+ginzatron@users.noreply.github.com>
2021-11-21 09:36:12 -0600

    starting fresh in angular

--- frontend/src/App.vue
+++
@@ -1,7 +0,0 @@
-<template>
-    <TheHeader msg="header message" class="q-pl-xl" />
-</template>
-
-<script setup lang="ts">
-import TheHeader from './components/layout/theHeader.vue'
-</script>
--- frontend/src/components/layout/theHeader.vue
+++
@@ -1,9 +0,0 @@
-<template>
-    <div>{{ msg }}</div>
-</template>
-
-<script setup lang="ts">
-const props = defineProps({
-    msg: { type: String, default: 'default message' },
-})
-</script>
