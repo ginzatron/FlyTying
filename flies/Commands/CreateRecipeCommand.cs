c11f30f92320147d333f7cbe884b193a53be2762
ginzatron <43377218+ginzatron@users.noreply.github.com>
2022-02-19 14:18:36 -0600

    created create fly endpoint

---
+++ flies/Commands/CreateRecipeCommand.cs
@@ -0,0 +1,26 @@
+﻿using Flies.RecipeDomain;
+using MediatR;
+using MongoDB.Driver;
+
+namespace Flies.Commands;
+
+public record CreateRecipeCommand(Recipe recipe) : IRequest<Task>;
+
+public class CreateRecipeCommandHandler : IRequestHandler<CreateRecipeCommand, Task>
+{
+    private readonly MongoDbContext _context;
+    protected IMongoCollection<Recipe> _collection;
+
+    public CreateRecipeCommandHandler(MongoDbContext context)
+    {
+        _context = context;
+        _collection = _context.GetCollection<Recipe>(typeof(Recipe).Name);
+    }
+    public Task<Task> Handle(CreateRecipeCommand request, CancellationToken cancellationToken)
+    {
+        request.recipe.CreatedAt = DateTime.UtcNow;
+        return Task.FromResult(_collection.InsertOneAsync(request.recipe));
+    }
+}
+
+
